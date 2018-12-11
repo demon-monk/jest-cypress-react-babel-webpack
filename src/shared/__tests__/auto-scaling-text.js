e8e154fc7ddeb63fbb8e64f04d8b9990465c8e2b
Demon Monk <antdude@126.com>
2018-12-11 23:39:02 +0800

    snapshot react component test

--- src/shared/__tests__/auto-scaling-text.js
+++ src/shared/__tests__/auto-scaling-text.js
@@ -4,5 +4,6 @@ import {render} from 'react-testing-library'
 import AutoScalingText from '../auto-scaling-text'
 
 test('renders', () => {
-    render(<AutoScalingText />)
+    const {container} = render(<AutoScalingText />)
+    expect(container).toMatchSnapshot()
 })
