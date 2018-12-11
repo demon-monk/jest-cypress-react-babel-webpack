90c62c15b3077045587250a0a5eec08a888762c8
Demon Monk <antdude@126.com>
2018-12-11 23:29:33 +0800

    jest snapshot update

--- src/shared/__tests__/auto-scaling-text.js
+++ src/shared/__tests__/auto-scaling-text.js
@@ -4,6 +4,5 @@ import {render} from 'react-testing-library'
 import AutoScalingText from '../auto-scaling-text'
 
 test('renders', () => {
-    const {container} = render(<AutoScalingText />)
-    console.log(container.innerHTML)
+    render(<AutoScalingText />)
 })
