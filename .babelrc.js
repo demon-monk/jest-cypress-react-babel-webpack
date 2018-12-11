93dc8c7ce0a67c49d1db5a8ddf541689fca1c6e5
Demon Monk <antdude@126.com>
2018-12-11 22:13:21 +0800

    add babel-test to support import in test enviroment

--- .babelrc.js
+++ .babelrc.js
@@ -1,24 +1,25 @@
 const isProd = String(process.env.NODE_ENV) === 'production'
 const isTest = String(process.env.NODE_ENV) === 'test'
-
-module.exports = {
-  presets: [
-    ['@babel/preset-env', {modules: isTest ? 'commonjs' : false}],
-    '@babel/preset-react',
+const presets = [
+  ['@babel/preset-env', {modules: isTest ? 'commonjs' : false}],
+  '@babel/preset-react',
+]
+const plugins = [
+  '@babel/plugin-syntax-dynamic-import',
+  '@babel/plugin-proposal-class-properties',
+  '@babel/plugin-proposal-object-rest-spread',
+  [
+    'babel-plugin-emotion',
+    {
+      hoist: isProd,
+      sourceMap: !isProd,
+      autoLabel: !isProd,
+      labelFormat: '[filename]--[local]',
+    },
   ],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    [
-      'babel-plugin-emotion',
-      {
-        hoist: isProd,
-        sourceMap: !isProd,
-        autoLabel: !isProd,
-        labelFormat: '[filename]--[local]',
-      },
-    ],
-    'react-loadable/babel',
-  ].filter(Boolean),
+  'react-loadable/babel',
+].filter(Boolean)
+module.exports = {
+  presets,
+  plugins,
 }
