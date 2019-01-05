bfe8da9cf6987793e5247d1123aa729f52c2b06b
demon-monk <antdude@126.com>
2019-01-05 22:54:39 +0800

    🚨 Test: a trivial way to test components that drived by provider

--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { render } from 'react-testing-library'
 import Calculator from '../calculator-display'
-
-expect.addSnapshotSerializer(createSerializer(emotion))
+import {ThemeProvider} from 'emotion-theming'
+import {dark} from '../../themes'
 
 test('renders calculator',  () => {
-    const {container} = render(<Calculator value="0" />)
+    const {container} = render(
+        <ThemeProvider theme={dark}>
+            <Calculator value="0" />
+        </ThemeProvider>
+    )
     expect(container.firstChild).toMatchSnapshot()
 })
