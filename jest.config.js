a39682eef4291343e49a8b39479b4ac0ab07b618
demon-monk <antdude@126.com>
2019-01-05 21:22:53 +0800

    ✨ Feat: add moduleDirectories in jest.config.js to resolve modules directly

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
+const path = require('path')
+
 module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     snapshotSerializers: ['jest-emotion'],
+    moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
     moduleNameMapper: {
         '\\.module\\.css$': 'identity-obj-proxy',
         '\\.css$': require.resolve('./test/style-mock.js'),
