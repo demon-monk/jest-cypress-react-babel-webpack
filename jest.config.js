d6f385081fc90ed9c3e560d84c2d63883a272c80
demon-monk <antdude@126.com>
2019-01-05 20:43:21 +0800

    ✨ Feat: extract common part to setup-tests.js and load it once jest is loaded use "setupTestFramewor

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,10 @@ module.exports = {
     moduleNameMapper: {
         '\\.module\\.css$': 'identity-obj-proxy',
         '\\.css$': require.resolve('./test/style-mock.js'),
-    }
+    },
+    // before jest is loaded
+    // testFiles: [],
+    // after jest is loaded
+    setupTestFrameworkScriptFile: require.resolve('./test/setup-tests.js'),
+
 }
